141f77b9fb8cb7c98bd4d783e8d4c1c18224585d
hitanga <hitanga@gmail.com>
2025-09-24 14:34:27 +0000

    Data Types

---
+++ basics/dataTypes.js
@@ -0,0 +1,22 @@
+ "use strict" //treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log( 3 + 3) // code readability should be high
+console.log("Gopal")
+
+let name = "Gopal"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint = BigInt is a special numeric type used to represent integers larger than the safe limit of Number (greater than 2⁵³ - 1).
+// string => "" //string is a sequence of characters enclosed in single quotes (''), double quotes (""), or backticks (`).
+// boolean = true/false
+// null = standalone value
+// undefined => value has been declared but not assisgned any value yet.
+// symbol = unique     
+// object = an object is a collection of key-value pairs where keys are strings (or Symbols) and values can be any data type.
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
